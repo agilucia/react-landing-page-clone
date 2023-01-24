935050706af4f26304e0bdca5a4f34ca159942f7
Agnes Pilar <agnes.pilar@yahoo.de>
2023-01-24 15:31:33 +0100

    Build html base structure

--- src/App.js
+++ src/App.js
@@ -59,8 +59,99 @@ function App() {
             location on every new tab.
           </p>
         </section>
+        <section>
+          <img src="/#" alt="tabImage" />
+        </section>
+        <section>
+          <div>
+            <img src="/#" alt="spaceImageAlt" />
+            <div>
+              <h2>Space images.</h2>
+              <p>
+                Every day get a fascinating image of our universe. Enjoy
+                pictures of stars, planets, moons, galaxies, nebulae, comets and
+                more right on your browser tab.
+              </p>
+              <a href="/#">+ Add to Chrome</a>
+            </div>
+          </div>
+          <div>
+            <div>
+              <h2>Sun Widget.</h2>
+              <p>
+                Find out when the sunrise, sunset, first and last light times
+                will be precisely at your location. It also shows a countdown in
+                an easy-to-read display, so you can stay tuned in your favorite
+                sun event - light up your world!.
+              </p>
+              <a href="/#">+ Add to Chrome</a>
+            </div>
+            <img src="/#" alt="sunWidgetImage" />
+          </div>
+          <div>
+            <img src="/#" alt="moonWidgetImage" />
+            <div>
+              <h2>Moon Widget.</h2>
+              <p>
+                See the current moon phase in real-time, countdowns to moon
+                events (New Moon, First Quarter, Full Moon and Last Quarter),
+                along with exact illumination percentages. Never miss an
+                opportunity for moon gazing anymore.
+              </p>
+              <a href="/#">+ Add to Chrome</a>
+            </div>
+          </div>
+          <div>
+            <div>
+              <h2>Inspire Yourself Daily.</h2>
+              <p>
+                Receive a daily dose of inspiration, insight and positive action
+                with our curated list of quotes from the most intelligent people
+                in history.
+              </p>
+              <a href="/#">+ Add to Chrome</a>
+            </div>
+            <img src="/#" alt="dailyInspirationImage" />
+          </div>
+          <div>
+            <img src="/#" alt="customizableInterfaceImage" />
+            <div>
+              <h2>Customizable Interface.</h2>
+              <p>
+                You can customize your experience with Spatium by showing and
+                hiding widgets as per your needs. You decide what's on your New
+                Tab page - it's up to you!.
+              </p>
+              <a href="/#">+ Add to Chrome</a>
+            </div>
+          </div>
+        </section>
       </main>
-      <footer>my footer</footer>
+      <footer>
+        <div>
+          <div>Spatium</div>
+          <div>
+            <div>
+              <h2>Be the first to know. No spam.</h2>
+              <p>
+                Join us and share our passion for the universe!. Stay in the
+                loop about new product updates, features and announcements.
+              </p>
+            </div>
+            <div>
+              <form>
+                <input></input>
+                <button>Subscribe</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div>
+          <a href="/#">
+            <img src="/#" alt="footerImage" />
+          </a>
+        </div>
+      </footer>
     </>
   );
 }
